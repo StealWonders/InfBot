7956053524649eba3af196cf31ce3f70c3ece4e4
maanex <andreas@maanex.me>
2021-10-19 22:42:34 +0200

    Kleinigkeiten ;)

--- src/rss/FeedChecker.ts
+++ src/rss/FeedChecker.ts
@@ -12,7 +12,7 @@ export default class FeedChecker {
         this.infBot = infBot;
     }
 
-    public start(): void {
+    public start() {
         Scheduler.scheduleJob(this.infBot.configuration.rsspollinterval, () => {
             for (const feed of this.infBot.configuration.feeds) {
                 console.log("Checking: " + feed.url);
@@ -27,54 +27,53 @@ export default class FeedChecker {
         }
     }
 
-    public async checkRSS(feed: Feed): Promise<void> {
+    public async checkRSS(feed: Feed) {
         const parser = new Parser();
         const parsedFeed = await parser.parseURL(feed.url);
 
-        const maxDate: Date = new Date();
+        const maxDate = new Date();
         maxDate.setDate(maxDate.getDate() - this.infBot.configuration.historyfor);
 
-        let embeds: MessageEmbed[] = [];
+        const embeds: MessageEmbed[] = [];
 
         for (const item of parsedFeed.items) {
             const resourceIcon: string = this.getResourceIcon(item);
-            if (resourceIcon != undefined) { // Only send non forum items
+            if (!resourceIcon) continue // Only send non forum items
 
-                // Skip feed entry as it's original date is too far back
-                const originDate = new Date(item.isoDate);
-                if (originDate < maxDate) break; // If the current item is already past the max date then dont check the rest
+            // Skip feed entry as it's original date is too far back
+            const originDate = new Date(item.isoDate);
+            if (originDate < maxDate) break; // If the current item is already past the max date then dont check the rest
 
-                const parsedItem: ParsedItem = this.parseItem(item);
-                if (parsedItem == undefined) continue; // If the item is unable to be parsed, skip
+            const parsedItem = this.parseItem(item);
+            if (!parsedItem) continue; // If the item is unable to be parsed, skip
 
-                //Check if the message is already sent (by checking storage)
-                if (this.infBot.messageStorage.contains(feed, item)) continue;
-                this.infBot.messageStorage.store(feed, item);
+            //Check if the message is already sent (by checking storage)
+            if (this.infBot.messageStorage.contains(feed, item)) continue;
+            this.infBot.messageStorage.store(feed, item);
 
-                // Item is not in storage → Send message
-                let fileName: string = parsedItem.fileName;
-                fileName = fileName.replace("/*/g", "\\*");
-                fileName = fileName.replace("/_/g", "\\_");
+            // Item is not in storage → Send message
+            let fileName: string = parsedItem.fileName;
+            fileName = fileName.replace("/*/g", "\\*");
+            fileName = fileName.replace("/_/g", "\\_");
 
-                const embed: MessageEmbed = new MessageEmbed()
-                    .setTitle(fileName + " " + (parsedItem.statusIcon || ''))
-                    .setDescription(parsedItem.filePath)
-                    .setColor(parsedItem.subject.color)
-                    .setTimestamp(originDate)
-                    .setThumbnail(parsedItem.subject.iconUrl)
-                    .setAuthor(parsedItem.subject.name)
-                    .setURL(item.link);
+            const embed: MessageEmbed = new MessageEmbed()
+                .setTitle(fileName + " " + (parsedItem.statusIcon || ''))
+                .setDescription(parsedItem.filePath)
+                .setColor(parsedItem.subject.color)
+                .setTimestamp(originDate)
+                .setThumbnail(parsedItem.subject.iconUrl)
+                .setAuthor(parsedItem.subject.name)
+                .setURL(item.link);
 
-                embeds.push(embed);
-            }
+            embeds.push(embed);
         }
 
         // embeds = embeds.reverse();
 
-        let messages: any = [];
+        const messages: MessageEmbed[][] = [];
 
         while (embeds.length > 0) {
-            let message: MessageEmbed[] = [];
+            const message: MessageEmbed[] = [];
             while (message.length < 10 && embeds.length > 0) {
                 message.push(embeds.pop());
             }
@@ -103,26 +102,26 @@ export default class FeedChecker {
     }
 
     private getResourceIcon(item: Item): string {
-        const target: string = new URL(item.link).searchParams.get('target');
-        const targetType: string = target.split('_')[0]; // https://www.studon.fau.de/studon/goto.php? client_id=StudOn & target=file_3703555
+        const target = new URL(item.link).searchParams.get('target');
+        const targetType = target.split('_')[0]; // https://www.studon.fau.de/studon/goto.php? client_id=StudOn & target=file_3703555
         return this.infBot.configuration.resourcetypes[targetType];
     }
 
     private parseItem(item: Item): ParsedItem {
-        const split: string[] = item.title.split('] ');
-        const nameAndStatus: string[] = split[split.length - 1].split(': ');
-        const fileName: string = nameAndStatus[0];
-        const fileStatusIdentifier: string = nameAndStatus[1];
+        const split = item.title.split('] ');
+        const nameAndStatus = split[split.length - 1].split(': ');
+        const fileName = nameAndStatus[0];
+        const fileStatusIdentifier = nameAndStatus[1];
 
         split.splice(split.length - 1);
-        const filePath: string = split.join('] ').substring(1);
-        const subjectIdentifier: string = filePath.split(' > ')[0];
+        const filePath = split.join('] ').substring(1);
+        const subjectIdentifier = filePath.split(' > ')[0];
         
-        const subject: Subject = this.infBot.configuration.subjects[subjectIdentifier];
-        const statusIcon: string = this.infBot.configuration.filestatus[fileStatusIdentifier];
+        const subject = this.infBot.configuration.subjects[subjectIdentifier];
+        const statusIcon = this.infBot.configuration.filestatus[fileStatusIdentifier];
 
-        if (subject == undefined || fileName == undefined || filePath == undefined) return undefined;
+        if (!subject || !fileName || !filePath) return undefined;
 
-        return {subject, fileName, statusIcon, filePath};
+        return { subject, fileName, statusIcon, filePath };
     }
 }
